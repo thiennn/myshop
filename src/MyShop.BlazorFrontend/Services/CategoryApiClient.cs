c8536f5312e955181e9e41f19fb88fe51ee08c37
To Quoc Bao <bao2703@gmail.com>
2020-05-02 01:33:48 +0700

    Upgrade to RC1

---
+++ src/MyShop.BlazorFrontend/Services/CategoryApiClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using MyShop.ViewModels;
+
+namespace MyShop.BlazorFrontend.Services
+{
+    public class CategoryApiClient : ICategoryApiClient
+    {
+        private readonly HttpClient _client;
+
+        public CategoryApiClient(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<IList<CategoryVm>> GetCategories()
+        {
+            var response = await _client.GetAsync("api/categories");
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<IList<CategoryVm>>();
+        }
+    }
+}
---
+++ src/MyShop.BlazorFrontend/Services/ICategoryApiClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MyShop.ViewModels;
+
+namespace MyShop.BlazorFrontend.Services
+{
+    public interface ICategoryApiClient
+    {
+        Task<IList<CategoryVm>> GetCategories();
+    }
+}
